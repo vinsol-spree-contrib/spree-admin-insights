06b562212daed1eccf2cba3b9a27bdc464c91884
Harsh Agrawal <harsh@vinsol.com>
2018-04-05 16:57:25 +0530

    Fix reports considering inventory units

--- app/reports/spree/best_selling_products_report.rb
+++ app/reports/spree/best_selling_products_report.rb
@@ -18,6 +18,37 @@ module Spree
     end
 
     def report_query
+      if Spree.version.to_f >= 3.3
+        return query_with_inventory_unit_quantities
+      else
+        return query_without_inventory_unit_quantities
+      end
+    end
+
+    private def search_name
+      search[:name].present? ? "%#{ search[:name] }%" : '%'
+    end
+
+    private def query_with_inventory_unit_quantities
+      Spree::LineItem
+        .joins(:order)
+        .joins(:variant)
+        .joins(:product)
+        .joins(:inventory_units)
+        .where(Spree::Product.arel_table[:name].matches(search_name))
+        .where(spree_orders: { state: 'complete' })
+        .where(spree_orders: { completed_at: reporting_period })
+        .where.not(spree_inventory_units: { state: 'returned' })
+        .group(:variant_id, :product_name, :product_slug, 'spree_variants.sku')
+        .select(
+          'spree_products.name        as product_name',
+          'spree_products.slug        as product_slug',
+          'spree_variants.sku         as sku',
+          'sum(spree_inventory_units.quantity) as sold_count'
+        )
+    end
+
+    private def query_without_inventory_unit_quantities
       Spree::LineItem
         .joins(:order)
         .joins(:variant)
@@ -35,10 +66,5 @@ module Spree
           'count(spree_line_items.id) as sold_count'
         )
     end
-
-    private def search_name
-      search[:name].present? ? "%#{ search[:name] }%" : '%'
-    end
-
   end
 end
